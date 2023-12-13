d54052758fecf721e6d988e648073c4ef8a7dab4
99prabh <133125732+99prabh@users.noreply.github.com>
2023-12-13 12:53:28 -0500

    User model details updated

--- MuseumFusion.Model/User.cs
+++ MuseumFusion.Model/User.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using Realms;
 
 namespace MuseumFusion.Model;
 
-public class User
+public class User : RealmObject
 {
+    [PrimaryKey]
+    [MapTo("_id")]
+    public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+
+    [MapTo("name")]
+    [Required]
     public string Name { get; set; }
+
+    [MapTo("email")]
+    [Required]
     public string Email { get; set; }
+
+    [MapTo("password")]
+    [Required]
     public string Password { get; set; }
+
+    [MapTo("is_admin")]
     public bool IsAdmin { get; set; }
 }
 
